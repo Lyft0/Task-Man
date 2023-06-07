0226336fc6fe1f0053c58282c8bc6bd2c9ee09a2
Lyft0 <soniva2002@gmail.com>
2023-06-07 11:40:56 +0700

    adding sign up layout screen

--- app/src/main/java/com/example/taskman/IntroActivity.kt
+++ app/src/main/java/com/example/taskman/IntroActivity.kt
@@ -1,15 +1,22 @@
 package com.example.taskman
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-    }
+        // Pindah ke register screen
+        val btnRegister = findViewById<Button>(R.id.btn_register_intro)
+        btnRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
 
+    }
 
 }
--- app/src/main/java/com/example/taskman/RegisterActivity.kt
+++ app/src/main/java/com/example/taskman/RegisterActivity.kt
@@ -2,10 +2,25 @@ package com.example.taskman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        setupActionBar()
+    }
+
+    private fun setupActionBar(){
+        val toolbarRegister = findViewById<Toolbar>(R.id.toolbar_register)
+        setSupportActionBar(toolbarRegister)
+
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_arrow_back)
+        }
+
+        toolbarRegister.setNavigationOnClickListener { onBackPressed() }
     }
 }
