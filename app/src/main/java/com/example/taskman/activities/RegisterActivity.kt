9aed8e4f9892a5bbb44a20be21590fdba5f2a35e
Lyft0 <soniva2002@gmail.com>
2023-06-09 06:55:54 +0700

    registeractivity added function to validate, setup new app on firebase

--- app/src/main/java/com/example/taskman/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/taskman/activities/RegisterActivity.kt
@@ -2,10 +2,14 @@ package com.example.taskman.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.taskman.R
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -23,5 +27,50 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         toolbarRegister.setNavigationOnClickListener { onBackPressed() }
+
+        val btnRegister = findViewById<Button>(R.id.btn_register)
+        btnRegister.setOnClickListener {
+            registerUser()
+        }
+    }
+
+    // register user
+    private fun registerUser(){
+        val fieldName = findViewById<EditText>(R.id.field_name)
+        val fieldEmail = findViewById<EditText>(R.id.field_email)
+        val fieldPass = findViewById<EditText>(R.id.field_password)
+
+        val name: String = fieldName.text.toString().trim { it <= ' ' }
+        val email: String = fieldEmail.text.toString().trim { it <= ' ' }
+        val password: String = fieldPass.text.toString().trim { it <= ' ' }
+
+        if (validateForm(name, email, password)) {
+            Toast.makeText(
+                this@RegisterActivity,
+                "Now we can register a new user.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    // validasi form
+    private fun validateForm(name: String, email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter name.")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter email.")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter password.")
+                false
+            }
+            else -> {
+                true
+            }
+        }
     }
 }
